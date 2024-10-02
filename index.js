9b7b5e78230d246c88acc079c372ce1daa702306
NazarKutoviy <nazarkutoviy@gmail.com>
2024-10-02 18:25:44 +0300

    add

---
+++ index.js
@@ -0,0 +1,57 @@
+// 1
+// const numbers =[1, 2, 3];
+// numbers[1]=10;
+// console.log(numbers)
+// 2
+// const matrix =[
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+// matrix[3]=[10, 11, 12];
+// console.log(matrix)
+// 3
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// let total = 0;
+// for (let i = 0; i < matrix.length; i += 1) {
+//   console.log("підмасив матриці matrix[i]:", matrix[i]);
+//   for (let j = 0; j < matrix[i].length; j += 1) {
+//     console.log("підмасив матриці matrix[i][j]:", matrix[i][j]);
+//     total += matrix[i][j];
+//   }
+// }
+// console.log(total)
+// 4
+// const numbers = [1, 2, 3, 5, 4];
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(numbers[i]);
+// }
+// 5
+// const matrix = [
+//   [1, 2, 3],
+//   [47, 4, 5, 6],
+//   [7, 8, 9, 5, 7, 6],
+//   [1,34, 24, 3],
+//   [4, 5, 8, 6, 6],
+//   [7,78, 8,4,5, 4,9],
+// ];
+// for (let i = 0; i < matrix.length; i += 1) {
+//   if(matrix[i].length>5) console.log(matrix[i]);
+// }
+// 6
+// const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
+// let maxNumber = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] > maxNumber) maxNumber = numbers[i];
+// }
+// console.log(maxNumber);
+// 7
+// const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] % 2 === 0) console.log(numbers[i]);
+// }
